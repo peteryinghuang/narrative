db5ae5d819da3c1a61df101a30b33113008fe593
Peter <139604770+peteryinghuang@users.noreply.github.com>
2024-04-23 17:09:23 -0400

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,4 @@
-/*!
- * jQuery JavaScript Library v1.10.2
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-07-03T13:48Z
- */
+
 (function( window, undefined ) {
 
 // Can't do this because several apps including ASP.NET trace
@@ -999,16 +987,7 @@ function isArraylike( obj ) {
 
 // All jQuery objects should point back to these
 rootjQuery = jQuery(document);
-/*!
- * Sizzle CSS Selector Engine v1.10.2
- * http://sizzlejs.com/
- *
- * Copyright 2013 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-07-03
- */
+
 (function( window, undefined ) {
 
 var i,
